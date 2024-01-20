882266f9c3dfc81ed603c146bb3d03db2092d3aa
SSOURABH58 <--g>
2024-01-21 01:27:57 +0530

    added formic

--- packages/ui/src/pages/SupportAgentForm.js
+++ packages/ui/src/pages/SupportAgentForm.js
@@ -1,14 +1,6 @@
 import React, { useState } from 'react';
 
 const SupportAgentForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [description, setDescription] = useState('');
-
-    const handleSubmit = async () => {
-        // Implement API call to create support agent
-    };
 
     return (
         <form onSubmit={handleSubmit}>
