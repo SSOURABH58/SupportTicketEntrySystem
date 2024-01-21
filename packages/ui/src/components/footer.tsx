50c962f1c9d556df4bee13425cb6b2e481246d89
SSOURABH58 <--g>
2024-01-21 06:29:44 +0530

    fixed footer

--- packages/ui/src/components/footer.tsx
+++ packages/ui/src/components/footer.tsx
@@ -6,10 +6,18 @@ const Footer = () => {
       <p>
         Made with <span className="text-danger">&hearts;</span> by Sourabh Soni
       </p>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://github.com/SSOURABH58"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         GitHub
       </a>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.linkedin.com/in/sourabh58/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Linked In
       </a>
     </footer>
